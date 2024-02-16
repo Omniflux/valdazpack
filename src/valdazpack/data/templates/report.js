7104d04605819b9d2cf4c5828c02497f6076a3e7
Omni Flux <omniflux@omniflux.com>
2024-02-16 13:43:55 -0700

    Remove HTML report formatting for previously removed full extension thumbnail issue

--- src/valdazpack/data/templates/report.js
+++ src/valdazpack/data/templates/report.js
@@ -8,9 +8,6 @@ window.addEventListener('DOMContentLoaded', (event) => {
 	[...document.querySelectorAll("[data-issue='AtypicalImageFilesInTexturesDirectoryIssue'] .issue-detail, [data-issue='AtypicalImageFilesInTemplatesDirectoryIssue'] .issue-detail")].forEach(detail => {
 		[...detail.getElementsByTagName('li')].forEach(li => { li.innerHTML = li.innerHTML.replace(/([^\/\.]+$)/m, '<span class="extension">$&</span>'); });
 	});
-	[...document.querySelectorAll("[data-issue='FullExtensionThumbnailsIssue'] .issue-detail")].forEach(detail => {
-		[...detail.getElementsByTagName('li')].forEach(li => { li.innerHTML = li.innerHTML.replace(/(\.[^\/\.]+)(?=\.png$)/i, '<span class="extension">$&</span>'); });
-	});
 	[...document.querySelectorAll("[data-issue='FullExtensionTipFilesIssue'] .issue-detail")].forEach(detail => {
 		[...detail.getElementsByTagName('li')].forEach(li => { li.innerHTML = li.innerHTML.replace(/(\.[^\/\.]+)(?=\.tip\.png$)/i, '<span class="extension">$&</span>'); });
 	});
